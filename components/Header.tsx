e084a021e81504e0381a04d0531e1d28adeac3a8
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-10-13 05:09:34 +0000

    pad

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 const Header = () => {
   return (
-    <header className="py-6 xl:py-6 ">
+    <header className="pt-6 xl:py-6 ">
       <div className="container mx-auto flex justify-between items-center ">
         {/* logo */}
         <Link href="/" className="flex justify-center items-center">
